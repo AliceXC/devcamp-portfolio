c6948003534fc169be5d445f2246cda15235dcd3
kevin.fan <kevin.fan@wrnch.ai>
2019-03-06 16:25:24 -0500

    integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,6 @@
 class PortfoliosController < ApplicationController
+    def index
+        @portfolio_items = Portfolio.all
+        #calling the model inside of the controller and make it available for the view
+    end
 end
