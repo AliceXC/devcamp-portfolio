849fc6be3dae2eb118958989cfed643f5c341d39
kevin.fan <kevin.fan@wrnch.ai>
2019-03-06 17:36:59 -0500

    integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,26 @@
 class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
-        #calling the model inside of the controller and make it available for the view
+        #calling the model inside of the controller 
+        #and make it available for the view
     end
+
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    def create
+        # weird ? params.require(:portfolio_item or :portfolio) both worked?
+        # updated only :portfolio works
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: "your portfolio item is created."}
+            else
+                format.html { render :new}
+            end
+        end
+    end
+
 end
