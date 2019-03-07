2a18ae54cfbc43a3cdb32aba2a97c10b0e6fc8d3
kevin.fan <kevin.fan@wrnch.ai>
2019-03-07 15:17:18 -0500

    integrated friendly routes for blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,7 @@
 class Blog < ApplicationRecord
+    extend FriendlyId
+    # title is the title of the blog, we can enter whatever blog,
+    # when hit save, it gonna generate a slug for us by taking the title
+    # and converting that.
+    friendly_id :title, use: [:slugged]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :portfolios, except [:show]
+  resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # we can customize the get 'any_str'
