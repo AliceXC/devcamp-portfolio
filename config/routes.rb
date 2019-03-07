72e136a4975d0d8f54787bf4afac9261803aaa25
kevin.fan <kevin.fan@wrnch.ai>
2019-03-07 12:28:37 -0500

    configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  # we can customize the get 'any_str'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # every time make new change to routes file, we have to restart the server
+  # setting is root path, im setting the home page
+  root to: 'pages#home' 
+
 end
