9243039baa0d545810568cf192d91a8076e62706
kevin.fan <kevin.fan@wrnch.ai>
2019-03-08 15:27:44 -0500

    implemented concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,5 +2,12 @@ class Skill < ApplicationRecord
 =begin
     here has direct connection to database
 =end 
-validates_presence_of :title, :percent_utilized
+    include Placeholder
+    validates_presence_of :title, :percent_utilized
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+    end
 end
