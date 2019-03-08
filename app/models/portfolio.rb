e1ec19b4ad20003f043b3dd49e6b0772c10a4650
kevin.fan <kevin.fan@wrnch.ai>
2019-03-08 14:29:08 -0500

    implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    # self is referencing the current version of Portfolio
+    def self.angular
+        where(subtitle: "angular")
+    end
+
+    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'ruby on rails') }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
+  get 'ruby-on-rails-items', to: 'portfolios#ruby_on_rails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # we can customize the get 'any_str'
