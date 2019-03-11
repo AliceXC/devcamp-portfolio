4ce37c211b31b22c86fd6cbdb5456ffb3a9729ae
kevin.fan <kevin.fan@wrnch.ai>
2019-03-11 15:40:33 -0400

    refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,12 +16,13 @@ class PortfoliosController < ApplicationController
 
     def new
         @portfolio_item = Portfolio.new
+        3.times { @portfolio_item.technologies.build}
     end
 
     def create
         # weird ? params.require(:portfolio_item or :portfolio) both worked?
         # updated only :portfolio works
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(portfolio_params)
 
         respond_to do |format|
             if @portfolio_item.save
@@ -39,7 +40,7 @@ class PortfoliosController < ApplicationController
     def update
         @portfolio_item = Portfolio.find(params[:id])
         respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          if @portfolio_item.update(portfolio_params)
             format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
           else
             format.html { render :edit }
@@ -64,5 +65,15 @@ class PortfoliosController < ApplicationController
         respond_to do |format|
             format.html { redirect_to portfolios_url, notice: 'Record was removed!'}
         end
+    end
+
+    private
+
+    def portfolio_params
+        params.require(:portfolio).permit(:title,
+                                        :subtitle,
+                                        :body,
+                                        technologies_attributes: [:name]
+                                        )
     end
 end
