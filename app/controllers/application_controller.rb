569a6faec1cd724c43d08d97842e6792327776ef
kevin.fan <kevin.fan@wrnch.ai>
2019-03-11 15:09:46 -0400

    added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include DeviseWhitelist
+
+    before_action :set_source
+
+    def set_source
+        # set this only if params[:q] exists
+        session[:source] = params[:q] if params[:q]
+    end
 end
